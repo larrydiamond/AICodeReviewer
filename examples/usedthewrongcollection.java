35a8604db6b10331cbd794ff37b5c7e5eb4e94e8
Larry Diamond <ldiamond@ldiamond.com>
2025-01-25 19:09:52 -0500

    should have used a hashmap example

---
+++ examples/usedthewrongcollection.java
@@ -0,0 +1,21 @@
+public class usedthewrongcollection {
+    public String findMostCommonString (final Set<String> strings) {
+        Map<String, Integer> counts = new TreeMap<>();
+        for (String s : strings) {
+            if (counts.containsKey(s)) {
+                counts.put(s, counts.get(s) + 1);
+            } else {
+                counts.put(s, 1);
+            }
+        }
+        int max = 0;
+        String mostCommon = null;
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                mostCommon = entry.getKey();
+            }
+        }
+        return mostCommon;
+    }
+}
